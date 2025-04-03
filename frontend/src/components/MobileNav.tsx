47c705118674346efd6a5e0851ec1ed80b523f1f
Nyesha <nyesha.gordon1@gmail.com>
2025-04-03 06:31:08 -0400

    Completed mobile login and out functionality

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,9 +1,12 @@
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet';
-import { Menu } from 'lucide-react';
+import { CircleUserRound, Menu } from 'lucide-react';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
+import { useAuth0 } from '@auth0/auth0-react';
+import MobileNavLinks from './MobileNavLinks';
 
 export default function MobileNav() {
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
       <Sheet>
         <SheetTrigger>
@@ -11,11 +14,23 @@ export default function MobileNav() {
         </SheetTrigger>
         <SheetContent className="bg-white flex flex-col p-5 py-8">
             <SheetTitle>
-                <span> Welcome to MegaBites.com!</span>
+                {isAuthenticated? (
+                    <span className="flex items-center font-bold gap-2">
+                        <CircleUserRound className="text-orange-500" />
+                        {user?.email}
+                    </span> 
+                ) : (
+                    <span> Welcome to MegaBites.com!</span>
+                )}
             </SheetTitle>
             <Separator className="my-3 border-t border-gray-200" />
-            <SheetDescription className="flex">
-                <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+            <SheetDescription className="flex flex-col gap-4">
+                {isAuthenticated ? (<MobileNavLinks/>
+                ) : (
+                    <Button 
+                    onClick={() => loginWithRedirect()}
+                    className="flex-1 font-bold bg-orange-500">Log In</Button>
+                ) } 
             </SheetDescription>
         </SheetContent>
       </Sheet>
