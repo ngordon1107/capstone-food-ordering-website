c0543fec8aa3cd560917505131cc9faedb4fad0f
Nyesha <nyesha.gordon1@gmail.com>
2025-04-04 14:09:47 -0400

    attempting to set up jwtParse for user profile api

--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser as any);
+
+router.put("/", jwtCheck, jwtParse,  MyUserController.updateCurrentUser);
 
 export default router;
