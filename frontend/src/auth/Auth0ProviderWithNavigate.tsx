ee87e658e37c9b84e93a86bb13608ed197660f72
Nyesha <nyesha.gordon1@gmail.com>
2025-04-03 20:52:52 -0400

    added auth0 access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
 import { useCreateMyUser } from '../api/MyUserApi';
+import { useNavigate } from 'react-router-dom';
 
 
 type Props = {
@@ -8,8 +9,7 @@ type Props = {
 };
 
 function Auth0ProviderWithNavigate({ children }: Props) {
-    const { createUser, isPending, isError, isSuccess } = useCreateMyUser();
-    
+    const navigate = useNavigate();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -20,9 +20,7 @@ function Auth0ProviderWithNavigate({ children }: Props) {
 
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        if (user?.sub && user?.email) {
-            createUser({ auth0Id: user.sub, email: user.email });
-        }
+        navigate("/auth-callback")
     };
 
     return (
